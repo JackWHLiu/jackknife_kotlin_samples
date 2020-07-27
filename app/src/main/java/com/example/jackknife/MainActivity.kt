58fcd39fc31df880892db99645969037ab1610be
Alm <924666990@qq.com>
2020-07-28 02:56:49 +0800

    更新Sample版本到jackknife-4.9.13

--- app/src/main/java/com/example/jackknife/MainActivity.kt
+++ app/src/main/java/com/example/jackknife/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jackknife
 import android.os.Bundle
 import com.example.jackknife.databinding.ActivityMainBinding
 import com.lwh.jackknife.BaseActivity
+import com.lwh.jackknife.util.NetworkUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -13,5 +14,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun getLayoutId(): Int {
         return R.layout.activity_main
+    }
+
+    //网络断开连接的回调
+    override fun onNetworkDisconnected() {
+    }
+
+    //网络连接成功的回调
+    override fun onNetworkConnected(type: NetworkUtils.ApnType?) {
     }
 }
