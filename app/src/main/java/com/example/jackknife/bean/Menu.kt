aaba85ff5bcacc871e3eab574cc2ce95f860a721
Alm <924666990@qq.com>
2020-08-23 22:19:11 +0800

    修改circleci配置

--- app/src/main/java/com/example/jackknife/bean/Menu.kt
+++ app/src/main/java/com/example/jackknife/bean/Menu.kt
@@ -5,14 +5,24 @@ import androidx.databinding.Bindable
 
 data class Menu(var id: Int, var title: String, var path: String) : BaseObservable() {
 
+//    /**
+//     * [android.widget.TextView.setText] )}时，如果给这个id，会找不到值，报错，因为这个id不是资
+//     * 源id。
+//     *
+//     * @return
+//     */
+//    @Bindable
+//    fun getId(): String {
+//        return id.toString()
+//    }
+
     /**
-     * [android.widget.TextView.setText] )}时，如果给这个id，会招不到值，报错，因为这个id不是资
-     * 源id。
+     * 兼容CircleCI的docker镜像的编译。
      *
      * @return
      */
     @Bindable
-    fun getId(): String {
+    fun getIdToString(): String {
         return id.toString()
     }
 }
